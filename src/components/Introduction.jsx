6d569e9c220d85107e22752e340548dc7f96ac28
Kevin <111733224+kevin21hk@users.noreply.github.com>
2023-12-02 03:49:15 +0800

    add introduction

---
+++ src/components/Introduction.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/no-unescaped-entities */
+// eslint-disable-next-line no-unused-vars
+import React from "react"
+
+const Introduction = () => {
+
+    return(
+    <>
+    <div className="introduction">
+    <h1>Harlo 👋</h1>
+    <h1>My name is <span className="name">Kevin</span></h1>
+    <h3>I'm a UK born Web Developer based in Hong Kong</h3>
+    </div>
+    </>
+    )
+}
+
+export default Introduction
